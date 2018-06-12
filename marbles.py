f96f38b7d7ba17d0fe91641507d655f5d8ed62d2
nagy135 <viktor.nagy1995@gmail.com>
2018-06-12 14:12:14 +0200

    Update marbles.py
    added player and hits

--- marbles.py
+++ marbles.py
@@ -19,6 +19,9 @@ TIME_STEP_SIZE = 0.02
 MARBLE_WEIGHT = 20
 SUN_WEIGHT = 400
 BIG_G = 1
+
+MARBLE_WIDTH = 5
+PLAYER_WIDTH = 10
 # os.environ['SDL_VIDEO_WINDOW_POS'] = "{},{}".format(0,0)
 
 class Game(object):
@@ -79,17 +82,44 @@ class Game(object):
         self.centers_of_gravity = list()
         self.centers_of_gravity.append((random.randint(110, WIDTH-111),random.randint(100,HEIGHT-111)))
 
+        self.player = [WIDTH//2, HEIGHT - 5]
+        self.hit = 0
+
+        self.x = tf.placeholder('float',[None, 400], name="self_x")
+        self.y = tf.placeholder('float', name="self_y")
+        self.classes = 4
+        self.keep_rate = 0.8
+        self.keep_prob = tf.placeholder(tf.float32)
+        self.training = True
+
     def draw(self):
         for marble in self.marbles:
-            pygame.draw.circle(self.gameDisplay, black, (int(marble.x), int(marble.y)), 5)
+            pygame.draw.circle(self.gameDisplay, black, (int(marble.x), int(marble.y)), MARBLE_WIDTH)
         for grav in self.centers_of_gravity:
             pygame.draw.circle(self.gameDisplay, red, (grav[0], grav[1]), 10)
+
         myfont = pygame.font.SysFont('Comic Sans MS', 30)
         textsurface = myfont.render(str(SUN_WEIGHT), False, (255, 0, 0))
         self.gameDisplay.blit(textsurface,(10,10))
+        textsurface = myfont.render(str(self.hit), False, (255, 0, 0))
+        self.gameDisplay.blit(textsurface,(WIDTH - 40,10))
+        
+        pygame.draw.circle(self.gameDisplay, green, (self.player[0], self.player[1]), PLAYER_WIDTH)
         # self.gameDisplay.fill(black, (STEP_SIZE*body_part[0], STEP_SIZE*body_part[1], STEP_SIZE, STEP_SIZE))
         # self.gameDisplay.fill(red, (self.food[0]*STEP_SIZE, self.food[1]*STEP_SIZE, STEP_SIZE, STEP_SIZE))
         # pygame.draw.line(self.gameDisplay, black, (10,10), (10, 30), 1)
+
+    def update_hits(self):
+        remove_arr = []
+        for i, marble in enumerate(self.marbles):
+            if self.distance_player_marble(marble) < (MARBLE_WIDTH + PLAYER_WIDTH):
+                self.hit += 1
+                remove_arr.append(i)
+        for i in remove_arr:
+            del self.marbles[i]
+
+    def distance_player_marble(self, marble):
+        return math.sqrt((self.player[0] - marble.x)**2 + (self.player[1] - marble.y)**2)
 
     def move(self):
 
@@ -97,6 +127,20 @@ class Game(object):
             for grav in self.centers_of_gravity:
                 marble.apply_gravity(grav[0], grav[1])
             marble.move()
+
+    def player_move(self, direction):
+        if direction == 'left':
+            if self.player[0] > 0:
+                self.player[0] -= 10
+        if direction == 'right':
+            if self.player[0] < WIDTH:
+                self.player[0] += 10
+        if direction == 'up':
+            if self.player[1] > 0:
+                self.player[1] -= 10
+        if direction == 'down':
+            if self.player[1] < HEIGHT:
+                self.player[1] += 10
 
     def tick(self):
         now = time.time()
@@ -111,14 +155,14 @@ class Game(object):
         while not self.end:
             for event in pygame.event.get():
                 if event.type == pygame.KEYUP:
-                #     if event.key == pygame.K_LEFT:
-                #         self.change_orientation('left')
-                #     if event.key == pygame.K_RIGHT:
-                #         self.change_orientation('right')
-                #     if event.key == pygame.K_UP:
-                #         self.change_orientation('up')
-                #     if event.key == pygame.K_DOWN:
-                #         self.change_orientation('down')
+                    if event.key == pygame.K_a:
+                        self.player_move('left')
+                    if event.key == pygame.K_d:
+                        self.player_move('right')
+                    if event.key == pygame.K_w:
+                        self.player_move('up')
+                    if event.key == pygame.K_s:
+                        self.player_move('down')
                     if event.key == pygame.K_p:
                         pause = not pause
                     if event.key == pygame.K_r:
@@ -140,6 +184,7 @@ class Game(object):
             if not pause:
                 self.tick()
             self.gameDisplay.fill(white)
+            self.update_hits()
             self.draw()
             pygame.display.update()
             self.clock.tick(60)
